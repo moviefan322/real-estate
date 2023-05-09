6b4878fb708f0344268ab2c449495dd93acdaafe
moviefan322 <philman202@yahoo.com>
2023-05-08 20:03:01 -0400

    adds leaflet map to listing

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import CreateListing from "./pages/CreateListing";
 import Listing from "./pages/Listing";
+import Contact from "./pages/Contact";
 
 function App() {
   if (typeof window !== "undefined") {
@@ -37,6 +38,7 @@ function App() {
             path="/category/:categoryName/:listingId"
             element={<Listing />}
           />
+          <Route path="/contact/:landlordId" element={<Contact />} />
         </Routes>
         <Navbar />
       </Router>
